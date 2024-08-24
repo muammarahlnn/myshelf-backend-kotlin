45a97413af141817f97573f472dfefbfd94e024c
muammarahlnn <21muammarahlan@gmail.com>
2024-08-24 16:35:46 +0800

    initial commit

---
+++ src/main/kotlin/com/muammarahlnn/myshelf/backend/MyshelfBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.muammarahlnn.myshelf.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyshelfBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyshelfBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/muammarahlnn/myshelf/backend/MyshelfBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.muammarahlnn.myshelf.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyshelfBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
